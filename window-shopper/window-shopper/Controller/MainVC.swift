4e72ca5753c25c4a377e1c467399eca8695ce6f8
ahmedhossam94 <medo_ah94@hotmail.com>
2018-01-19 16:07:07 +0200

    after drawing sympole

--- window-shopper/window-shopper/Controller/MainVC.swift
+++ window-shopper/window-shopper/Controller/MainVC.swift
@@ -12,36 +12,53 @@ class MainVC: UIViewController,UITextFieldDelegate {
 
     @IBOutlet weak var wgeTxt: RadText!
     @IBOutlet weak var pricetxt: RadText!
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var hourLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+       // make clcultae button by code
+       
         let calcButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
-      calcButton.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        calcButton.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
         calcButton.setTitle("Calculate", for: .normal)
         calcButton.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
         calcButton.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
         wgeTxt.inputAccessoryView = calcButton
         pricetxt.inputAccessoryView = calcButton
        
-        
+        resultLbl.isHidden = true
+        hourLbl.isHidden = true
     }
 
     
     
     @objc func calculate()  {
+       
+        if let wageTx = wgeTxt.text, let priceTx = pricetxt.text{
+            if let wage = Double (wageTx), let price = Double(priceTx){
+                resultLbl.isHidden = false
+                hourLbl.isHidden = false
+               view.endEditing(true)
+                resultLbl.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
+       
         
-        print("aaaaa")
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
     
-   /* func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        self.view.endEditing(true)
-        return true
+ 
+    @IBAction func clearCalcPressed(_ sender: Any) {
+        resultLbl.isHidden = true
+        hourLbl.isHidden = true
+        wgeTxt.text = ""
+        pricetxt.text = ""
     }
-*/
+    
 }
 
--- window-shopper/window-shopper/Views/RadText.swift
+++ window-shopper/window-shopper/Views/RadText.swift
@@ -10,13 +10,24 @@ import UIKit
 @IBDesignable
 class RadText: UITextField {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+    
     override func draw(_ rect: CGRect) {
-        // Drawing code
+
+        let size :CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height/2)-size/2, width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.8224352311, green: 0.8224352311, blue: 0.8224352311, alpha: 1)
+        currencyLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        currencyLabel.textAlignment = .center
+        currencyLabel.layer.cornerRadius = 5
+        currencyLabel.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLabel.text = formatter.currencySymbol
+        addSubview(currencyLabel)
+    
     }
-    */
+
     override func prepareForInterfaceBuilder() {
         customeView()
     }
@@ -32,6 +43,7 @@ class RadText: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2918985445)
         layer.cornerRadius = 5
         textAlignment = .center
+        clipsToBounds = true
         textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         if let p = placeholder {
             let placehold = NSAttributedString(string: p, attributes:[ .foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
